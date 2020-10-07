44b61ce7bf26e86e4af76c46223c5836d9003d7d
PamelaBarboza <pamelarabarboza@gmail.com>
2020-10-07 12:12:29 -0300

    creation of the whatsapp component

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './Home.css';
-import MenuNavegacion from './Componentes/ElementosFijos/MenuNavegacion';
-import Footer from './Componentes/ElementosFijos/Footer';
-import RedesSociales from './Componentes/ElementosFijos/RedesSociales';
-
+import React from "react";
+import "./Home.css";
+import Whatsapp from "./Componentes/ElementosFijos/Whatsapp";
+import MenuNavegacion from "./Componentes/ElementosFijos/MenuNavegacion";
+import Footer from "./Componentes/ElementosFijos/Footer";
+import RedesSociales from "./Componentes/ElementosFijos/RedesSociales";
 
 function Home() {
-  
   return (
     <div className="App">
       <MenuNavegacion />
+      <Whatsapp />
       <Footer />
       <RedesSociales />
-    </div >
+    </div>
   );
 }
 
