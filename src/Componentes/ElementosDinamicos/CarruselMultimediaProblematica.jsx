89038266f1e6c0a7deb6738ccc507c7c86363eb8
Amanda D <adnamadiaz@gmail.com>
2020-10-07 10:03:33 -0300

    Add video in component Carousel

--- src/Componentes/ElementosDinamicos/CarruselMultimediaProblematica.jsx
+++ src/Componentes/ElementosDinamicos/CarruselMultimediaProblematica.jsx
@@ -5,17 +5,23 @@ import imgcarousel1 from '../../img/imgcarousel1.jpg';
 import imgcarousel2 from '../../img/imgcarousel2.jpg';
 import imgcarousel3 from '../../img/imgcarousel3.jpg';
 
+import LaReceta from '../MultimediaComponents/LaReceta.mp4';
 
 const CarruselMultimediaProblematica = (props) => {
   return (
     <div>
       <Carousel>
         <Carousel.Item>
-          <img
+          <video className="video d-block w-100"
+            autoPlay muted
+            loop >
+            <source src={LaReceta} type="video/mp4" />
+          </video>
+          {/* <img
             className="d-block w-100"
             src={imgcarousel1}
             alt="First slide"
-          />
+          /> */}
           <Carousel.Caption>
             <h3>{props.tituloPrimerSlide}</h3>
             <p>{props.parrafoPrimerSlide}</p>
