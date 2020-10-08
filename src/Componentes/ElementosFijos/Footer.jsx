15961728a516aa8f42234b4fff36926a3b9bd896
PamelaBarboza <pamelarabarboza@gmail.com>
2020-10-08 13:06:20 -0300

    Edition footer

--- src/Componentes/ElementosFijos/Footer.jsx
+++ src/Componentes/ElementosFijos/Footer.jsx
@@ -1,31 +1,39 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-import logo from "../../img/logoblanco.png";
+import logo from "../../img/logonaranja.png";
 import RedesSociales from "./RedesSociales";
+import viñeta from "../../img/Viñeta naranja.png";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="pastillaFooter">
-      <div className="logoImg">
-        <img src={logo} />
-
-        <div>
-          <footer>
-            <ul>
-              <li>
-                <a href="/PreguntasFrecuentes">Preguntas frecuentes</a>
-              </li>
-              <li>
-                <a href="/DondeEstamos">¿Dónde Estamos?</a>
-              </li>
-              <li>
-                <a href="/Transparencia">Transparencia</a>
-              </li>
-            </ul>
-            <a href="/2020">©2020</a>
-            <RedesSociales/>
-          </footer>
-        </div>
+      <div>
+        <footer>
+          <ul>
+            <li>
+              <img className="viñeta" src={viñeta} />
+              <a href="/PreguntasFrecuentes">Preguntas frecuentes</a>
+            </li>
+            <li>
+              <img className="viñeta" src={viñeta} />
+              <a href="/DondeEstamos">¿Dónde Estamos?</a>
+            </li>
+            <li>
+              <img className="viñeta" src={viñeta} />
+              <a href="/Transparencia">Transparencia</a>
+            </li>
+          </ul>
+          <div className="logoImg">
+            <img src={logo} />
+          </div>
+          <RedesSociales />
+          <div ClassName="Año">
+            <a href="/2020" ClassName="Año">
+              ©2020 - Comida para todos, Santiago de Chile
+            </a>
+          </div>
+        </footer>
       </div>
     </div>
   );
