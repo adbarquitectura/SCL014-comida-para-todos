be89fb5f34b322e0e4d38fc757d7a1c8b75ca7f2
Amanda D <adnamadiaz@gmail.com>
2020-10-09 14:41:19 -0300

    Update styles for Redes Sociales component

--- src/Componentes/ElementosFijos/RedesSociales.jsx
+++ src/Componentes/ElementosFijos/RedesSociales.jsx
@@ -7,18 +7,12 @@ import twitter from "../../img/Twitter.png";
 
 const RedesSociales = () => {
   return (
-    <div className="Social-Container">
+    <div className="Social-Container" >
       <div className="SocialIcons">
-        <a
-          href="https://www.instagram.com/comida_para_todos_cl/"
-          className="Instagram Social"
-        >
+        <a href="https://www.instagram.com/comida_para_todos_cl/" className="Instagram Social">
           <img className="TamanoSocialIcons" src={instagram} />
         </a>
-        <a
-          href="https://www.facebook.com/Comida-para-Todos-100133851719302/"
-          className="Facebook Social"
-        >
+        <a href="https://www.facebook.com/Comida-para-Todos-100133851719302/" className="Facebook Social">
           <img className="TamanoSocialIcons" src={facebook} />
         </a>
         <a href="https://twitter.com/chilecomida" className="Twitter Social">
@@ -26,9 +20,10 @@ const RedesSociales = () => {
         </a>
       </div>
       <div className="SocialText">
-        <p className="ColorParrafo">¡Síguenos en las redes!</p>
+        <p className="ColorParrafo">¡Síguenos en las redes! </p>
       </div>
     </div>
   );
 };
+
 export default RedesSociales;
