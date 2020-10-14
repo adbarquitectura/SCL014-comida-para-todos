8c51fc1bb392006800a2e1506184d7e2fed39982
CatalinaJL <catalina.p.jl@gmail.com>
2020-10-14 12:51:40 -0300

    Add funcionality of MapaChile

--- src/Componentes/Mapa/MapaChile.jsx
+++ src/Componentes/Mapa/MapaChile.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { VectorMap } from '@south-paw/react-vector-maps';
 import Chile from './chile.json';
 import './MapaChile.css'
+import * as DondeEstan from './DondeEstan.js';
 
 const MapaChile = () => {
+   
+    /* Definición del Estado de select Comunas*/
+    const [comunas, setcomunas] = useState([]);
+
+    const onClick = ({ target }) => {
+        const name = target.attributes.name.value;
+        console.log (name);
+        if (name === 'Biobío') {
+            return setcomunas(DondeEstan.Biobío)
+        } else if (name === 'Antofagasta') {
+            return setcomunas(DondeEstan.Antofagasta)
+        } else if (name === 'Valparaíso') {
+            return setcomunas(DondeEstan.Valparaíso)}
+        else if (name === 'Metropolitana'){ 
+            return setcomunas(DondeEstan.Metropolitana) }
+    };
+
+
     return (
+
         <div>
-            < VectorMap classname= "chile-map" {...Chile} />
+            < VectorMap classname= "chile-map" {...Chile} layerProps={{ onClick }}/>
+
+            {comunas.map((comuna, index) => {
+                return (<p key={index} value={comuna}>{comuna}</p>)
+                 })}
         </div>
     )
 }
