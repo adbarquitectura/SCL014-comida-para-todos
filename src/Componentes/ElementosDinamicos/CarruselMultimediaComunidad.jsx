90ce7014ab971ef7994a11284897a9908a9d3ec3
PamelaBarboza <pamelarabarboza@gmail.com>
2020-10-14 20:04:27 -0300

    section quienes somos

--- src/Componentes/ElementosDinamicos/CarruselMultimediaComunidad.jsx
+++ src/Componentes/ElementosDinamicos/CarruselMultimediaComunidad.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import { Carousel } from 'react-bootstrap';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Carousel } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
-import NextIcon from '../Iconos/NextIcon';
-import PrevIcon from '../Iconos/PrevIcon';
+import NextIcon from "../Iconos/NextIcon";
+import PrevIcon from "../Iconos/PrevIcon";
 
 import imgcarrusel1 from "../../img/FotoAbuelita.png";
-import imgcarrusel2 from "../../img/images.jpg";
-import './CarruselMultimediaComunidad.css';
-
+import imgcarrusel2 from "../../img/yellow.jpg";
+import "./CarruselMultimediaComunidad.css";
 
 const CarruselMultimediaComunidad = (props) => {
   const [index, setIndex] = useState(0);
@@ -21,7 +20,9 @@ const CarruselMultimediaComunidad = (props) => {
     <div className="boxCarruselComunidad">
       <div className="boxTituloCarrusel">
         <div className="viñetaYellow"></div>
-        <a name={props.contenidoInfo}><h2 className="tituloalineadoIzquierda">{props.contenidoInfo}</h2></a>
+        <a name={props.contenidoInfo}>
+          <h2 className="tituloalineadoIzquierda">{props.contenidoInfo}</h2>
+        </a>
       </div>
 
       <div className="boxContenedorCarruselComunidad">
@@ -47,8 +48,9 @@ const CarruselMultimediaComunidad = (props) => {
                     <h3>Sra. Eugenia</h3>
                     <h4>Hogar de ancianos “El Remanso”</h4>
                     <p>
-                      Agradezco a esta iniciativa, ya que más que alimentarme el estomago me alimenta el corazón.
-                      La compañía de estas personas me da alegría y animos para el día a día
+                      Agradezco a esta iniciativa, ya que más que alimentarme el
+                      estomago me alimenta el corazón. La compañía de estas
+                      personas me da alegría y animos para el día a día.
                     </p>
                   </div>
                 </Col>
@@ -69,20 +71,22 @@ const CarruselMultimediaComunidad = (props) => {
 
                 <Col>
                   <div className="boxTextoCarruselAlianzas">
-                    <h3>Jorge Lopez</h3>
-                    <h4>Dueño Boa Restaurant</h4>
-                    <p>Como restaurant, estamos muy contentos de unirnos a esta causa y poder colaborar con platos de comida ricos y de calidad, cocinados por nuestros propios chefs y con mucho cariño para las comunidades.</p>
+                    <h3>Sr. Adrian Cardozo</h3>
+                    <h4>Comedor comunitario "Manos solidarias"</h4>
+                    <p>
+                      Cuando llego al centro comunitario con tantos platos
+                      llenos de comida tan rica y saludable se nota la felicidad
+                      de las personas, no hay nada mejor que tener la panza
+                      llena y el corazón contento.
+                    </p>
                   </div>
                 </Col>
               </Row>
             </Container>
           </Carousel.Item>
-
         </Carousel>
       </div>
-
-
-    </div >
+    </div>
   );
 };
 
--- src/Componentes/Vistas/Quienes-somos/QuienesSomosView.jsx
+++ src/Componentes/Vistas/Quienes-somos/QuienesSomosView.jsx
@@ -1,14 +1,91 @@
-import React from 'react';
+import React from "react";
+/* import "./RegalaUnAlmuerzo.css"; */
+import { Col, Container, Row } from "react-bootstrap";
+import ElEquipo from "../../../img/equipo.jpg";
+import Rafael from "../../../img/Ellipse 2.png";
+import Enrique from "../../../img/enrique-gellona.png";
+import Camila from "../../../img/Camila Moreno.png";
+import Maria from "../../../img/Maria jose e la fuente.png";
 
-const QuienesSomosView = () => {
-    return (
+const QuienesSomosView = (props) => {
+  return (
+    <div className="contenedorGeneralRegalaAlmuerzo">
+      <div className="boxTituloCarrusel">
+        <a name={props.contenidoInfo}>
+          <h2 className="tituloalineadoIzquierda">{props.contenidoInfo}</h2>
+        </a>
+      </div>
+      <div className="contenedorboxRegalaAlmuerzo">
+        <Container>
+          <Row>
+            <Col>
+              <div className="textoalineadoIzquierda">
+                <div className="viñetaYellow"></div>
+                <h3>Acerca de nosotros...</h3>
 
-        <div>
-            Estas en el módulo de “Quiénes somos”
-        </div >
+                <p>
+                  Trabajamos para ayudar a combatir el hambre en Chile, a través
+                  una alimentación de calidad y nutritiva; conectando,
+                  colaborando y generando asociatividad entre los actores de la
+                  cadena gastronómica y la ciudadanía "Comida para todos” es una
+                  plataforma solidaria que nace de la unión de organizaciones
+                  sociales, instituciones públicas, restaurantes, productores,
+                  que busca salvaguardar la Seguridad Alimentaria.
+                </p>
+              </div>
+            </Col>
 
-
-    )
-}
+            <Col>
+              <div className="fotoAlmuerzo">
+                <img src={ElEquipo} alt="Foto" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+        <Container>
+          <Row>
+            <Col>
+              <div className="textoalineadoIzquierda">
+                <div className="viñetaYellow"></div>
+                <h3>El equipo</h3>
+                <p>
+                  Somos un gran equipo de trabajo interdisciplinario y en
+                  conjunto nos esforzamos por que día a día se cumplan los
+                  objetivos de nuestra labor.
+                </p>
+              </div>
+            </Col>
+            <Row>
+              <Col>
+                <div className="fotoAlmuerzo2">
+                  <img src={Rafael} alt="Foto" />
+                  <p>Rafael Rincón Director y Cofundador </p>
+                </div>
+              </Col>
+              <Col>
+                <div className="fotoAlmuerzo2">
+                  <img src={Enrique} alt="Foto" />
+                  <p>Enrique Gellona Cofundador</p>
+                </div>
+              </Col>
+              <Col>
+                <div className="fotoAlmuerzo2">
+                  <img src={Camila} alt="Foto" />
+                  <p>Camila Moreno Cofundadora </p>
+                </div>
+              </Col>
+              <Col>
+                <div className="fotoAlmuerzo2">
+                  <img src={Maria} alt="Foto" />
+                  <p>María Jose de la Fuente Cofundadora </p>
+                </div>
+              </Col>
+            </Row>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
 export default QuienesSomosView;
