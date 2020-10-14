1e92f47b4d63dd9ff6dd696244717aeff87907c2
PamelaBarboza <pamelarabarboza@gmail.com>
2020-10-14 20:56:04 -0300

    last details of the home

--- src/Componentes/ElementosDinamicos/ContainerMultimediaHome.jsx
+++ src/Componentes/ElementosDinamicos/ContainerMultimediaHome.jsx
@@ -1,80 +1,74 @@
-import React from 'react';
-import videoCPT from '../MultimediaComponents/videofinalCPT.mp4';
+import React from "react";
+import videoCPT from "../MultimediaComponents/videofinalCPT.mp4";
 
-import './ContainerMultimediaHome.css';
-import Carousel from 'react-bootstrap/Carousel'
-import imgcarousel2 from '../../img/imgcarousel2.jpg';
-import imgcarousel3 from '../../img/imgcarousel3.jpg';
-import BotonLinkBlank from '../Botones/BotonLinkBlank';
-
+import "./ContainerMultimediaHome.css";
+import Carousel from "react-bootstrap/Carousel";
+import imgcarousel2 from "../../img/objeticos de desarrollo.jpg";
+import imgcarousel3 from "../../img/imgcarousel2.jpg";
+import BotonLinkBlank from "../Botones/BotonLinkBlank";
 
 /* Función Botón 'Regala un almuerzo' */
 const clickBton = () => {
-  console.log('estoy escuchando');
-  alert('Para realizar la donación te redireccionaremos a www.yodono.cl')
-  window.open('https://yodono.cl/proyecto/207/CultivaComidaparaTods')
-
-}
+  console.log("estoy escuchando");
+  alert("Para realizar la donación te redireccionaremos a www.yodono.cl");
+  window.open("https://yodono.cl/proyecto/207/CultivaComidaparaTods");
+};
 
 const ContainerMultimediaHome = () => {
   return (
     <div>
       <Carousel>
-
         <Carousel.Item>
-          <video
-            className="video"
-            autoPlay muted
-            loop >
+          <video className="video" autoPlay muted loop>
             <source src={videoCPT} type="video/mp4" />
           </video>
           <Carousel.Caption>
             <div className="Overlay">
-              <h1 className="colorTextoh1"> Ayúdanos a sumar más platos de comida en las mesas de los chilenos </h1>
+              <h1 className="colorTextoh1">
+                {" "}
+                Ayúdanos a sumar más platos de comida en las mesas de los
+                chilenos{" "}
+              </h1>
             </div>
-
           </Carousel.Caption>
         </Carousel.Item>
 
-
         <Carousel.Item>
-          <img
-            className="video"
-            src={imgcarousel2}
-            alt="Third slide"
-          />
+          <img className="video" src={imgcarousel2} alt="Third slide" />
 
           <Carousel.Caption>
             <div className="Overlay">
-              <h1 className="colorTextoh1"> Ayúdanos a sumar más platos de comida en las mesas de los chilenos </h1>
-              < BotonLinkBlank
-                contenidobtn={'Objetivos de desarrollo sostenible'}
-                anclaLinkTo={'https://www.un.org/sustainabledevelopment/es/hunger/'}
+              <h1 className="colorTextoh12">
+                {" "}
+                Comida para todos colabora con los siguientes objetivos de
+                desarrollo sostenible mundiales:{" "}
+              </h1>
+              <BotonLinkBlank
+                contenidobtn={"Objetivos de desarrollo sostenible"}
+                anclaLinkTo={
+                  "https://www.un.org/sustainabledevelopment/es/hunger/"
+                }
               />
             </div>
-
           </Carousel.Caption>
         </Carousel.Item>
 
         <Carousel.Item>
-          <img
-            className="video"
-            src={imgcarousel3}
-            alt="Third slide"
-          />
+          <img className="video" src={imgcarousel3} alt="Third slide" />
 
           <Carousel.Caption>
             <div className="Overlay">
-              <h1 className="colorTextoh1"> Ayúdanos a sumar más platos de comida en las mesas de los chilenos </h1>
-
+              <h1 className="colorTextoh1">
+                {" "}
+                Ayúdanos a combatir el hambre en Chile donando almuerzos
+                solidarios.{" "}
+              </h1>
             </div>
-
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </div>
-
-  )
-}
+  );
+};
 
 export default ContainerMultimediaHome;
--- src/Componentes/Vistas/Como-aportar/ComoAportarView.jsx
+++ src/Componentes/Vistas/Como-aportar/ComoAportarView.jsx
@@ -1,31 +1,28 @@
-import React, { useState } from 'react';
-import './ComoAportarView.css';
-import SeccionManerasAportar from '../../ElementosDinamicos/SeccionManerasAportar';
+import React, { useState } from "react";
+import "./ComoAportarView.css";
+import SeccionManerasAportar from "../../ElementosDinamicos/SeccionManerasAportar";
 
-import ModalFormularios from '../../Modales/ModalFormularios';
-import BotonGeneral from '../../Botones/BotonGeneral';
-import FormularioRestaurant from '../../Formularios/FormularioRestaurant';
-import FormularioMetroCuadrado from '../../Formularios/FormularioMetro2';
-import FormularioCapacitacion from '../../Formularios/FormularioCapacitación';
-import FormularioBancoAlimentos from '../../Formularios/FormularioBancoAlimentos';
-import FormularioTalento from '../../Formularios/FormularioTalento';
+import ModalFormularios from "../../Modales/ModalFormularios";
+import BotonGeneral from "../../Botones/BotonGeneral";
+import FormularioRestaurant from "../../Formularios/FormularioRestaurant";
+import FormularioMetroCuadrado from "../../Formularios/FormularioMetro2";
+import FormularioCapacitacion from "../../Formularios/FormularioCapacitación";
+import FormularioBancoAlimentos from "../../Formularios/FormularioBancoAlimentos";
+import FormularioTalento from "../../Formularios/FormularioTalento";
 import AportarRestaurante from "../../ElementosDinamicos/AportarRestaurante";
-import RegalaUnAlmuerzo from '../../ElementosDinamicos/RegalaUnAlmuerzo';
-import ManerasAportarSeccionDesp from '../../ElementosDinamicos/ManerasAportarSeccionDesp';
+import RegalaUnAlmuerzo from "../../ElementosDinamicos/RegalaUnAlmuerzo";
+import ManerasAportarSeccionDesp from "../../ElementosDinamicos/ManerasAportarSeccionDesp";
 
-import imgTalento from '../../../img/Maneras de aportar_Talento.jpg';
-import imgCapacitacion from '../../../img/Maneras de aportar_Capacita.jpg';
-import imgMetro from '../../../img/Maneras de aportar_Huerto.jpg';
-import imgBanco from '../../../img/Maneras de aportar_Banco.jpg';
+import imgTalento from "../../../img/Maneras de aportar 2_Mesa de trabajo 1 copia.jpg";
+import imgCapacitacion from "../../../img/Maneras de aportar 2_Mesa de trabajo 1 copia 3.jpg";
+import imgMetro from "../../../img/Maneras de aportar 2_Mesa de trabajo 1 copia 2.jpg";
+import imgBanco from "../../../img/Maneras de aportar 2_Mesa de trabajo 1 copia 4.jpg";
 
-import dataAportar from './dataComoAportar';
-
+import dataAportar from "./dataComoAportar";
 
 const clickBton = () => {
   window.open("https://yodono.cl/proyecto/207/CultivaComidaparaTods");
 };
-
-
 
 const ComoAportarView = () => {
   const [dataComoAportar, setDataComoAportar] = useState(dataAportar);
@@ -33,70 +30,113 @@ const ComoAportarView = () => {
   const [modalShow, setModalShow] = React.useState(false);
   //Maneja estado de boton
   const [contenidoModal, setContenidoModal] = useState();
-  const [tituloModal, setTituloModal] = useState('');
-  const [contenidoModalSecundario, setContenidoModalSecundario] = useState('');
+  const [tituloModal, setTituloModal] = useState("");
+  const [contenidoModalSecundario, setContenidoModalSecundario] = useState("");
 
   const [estadoboton, setEstadoboton] = useState(false);
 
-
   //Maneja estado de boton
-  const [styleBotonTalento, setStyleBotonTalento] = useState({ backgroundColor: '#FD4C00', border: 'none' });
-  const [styleBotonMetro, setStyleBotonMetro] = useState({ backgroundColor: '#FD4C00', border: 'none' });
-  const [styleBotonCapacitacion, setStyleBotonCapacitacion] = useState({ backgroundColor: '#FD4C00', border: 'none' });
-  const [styleBotonBanco, setStyleBotonBanco] = useState({ backgroundColor: '#FD4C00', border: 'none' });
-
-
-
+  const [styleBotonTalento, setStyleBotonTalento] = useState({
+    backgroundColor: "#FD4C00",
+    border: "none",
+  });
+  const [styleBotonMetro, setStyleBotonMetro] = useState({
+    backgroundColor: "#FD4C00",
+    border: "none",
+  });
+  const [styleBotonCapacitacion, setStyleBotonCapacitacion] = useState({
+    backgroundColor: "#FD4C00",
+    border: "none",
+  });
+  const [styleBotonBanco, setStyleBotonBanco] = useState({
+    backgroundColor: "#FD4C00",
+    border: "none",
+  });
 
   //Maneja estados para seccion de despliegue de formularios
-  const [imgFondoSeccion, setImgFondoSeccion] = useState({ backgroundImage: `url('${imgTalento}')` });
-  const [tituloSeccionDespliegue, setTituloSeccionDespliegue] = useState('Que tipo de Talento puedo ragalar');
-  const [parrafoSeccionDespliegue, setParrafoSeccionDespliegue] = useState('Los aportes desde los restaurantes son parte fundamental para que toda esta hermosa labor se vuelva realidad, los restaurantes colaboran, junto con nosotros a favor de las comunidades vulnerables, no solo llevándoles alimentos.');
-  const [funcionBotonSeccionDespliegue, setFuncionBotonSeccionDespliegue] = useState();
+  const [imgFondoSeccion, setImgFondoSeccion] = useState({
+    backgroundImage: `url('${imgTalento}')`,
+  });
+  const [tituloSeccionDespliegue, setTituloSeccionDespliegue] = useState(
+    "Que tipo de Talento puedo ragalar"
+  );
+  const [parrafoSeccionDespliegue, setParrafoSeccionDespliegue] = useState(
+    "Los aportes desde los restaurantes son parte fundamental para que toda esta hermosa labor se vuelva realidad, los restaurantes colaboran, junto con nosotros a favor de las comunidades vulnerables, no solo llevándoles alimentos."
+  );
+  const [
+    funcionBotonSeccionDespliegue,
+    setFuncionBotonSeccionDespliegue,
+  ] = useState();
 
   const changeEstadoBotonActiveTalento = () => {
-    setStyleBotonTalento({ backgroundColor: 'transparent', border: '2px solid #FD4C00', color: '#FD4C00' });
-    setStyleBotonMetro({ backgroundColor: '#FD4C00', border: 'none' });
-    setStyleBotonCapacitacion({ backgroundColor: '#FD4C00', border: 'none' });
-    setStyleBotonBanco({ backgroundColor: '#FD4C00', border: 'none' });
+    setStyleBotonTalento({
+      backgroundColor: "transparent",
+      border: "2px solid #FD4C00",
+      color: "#FD4C00",
+    });
+    setStyleBotonMetro({ backgroundColor: "#FD4C00", border: "none" });
+    setStyleBotonCapacitacion({ backgroundColor: "#FD4C00", border: "none" });
+    setStyleBotonBanco({ backgroundColor: "#FD4C00", border: "none" });
     setImgFondoSeccion({ backgroundImage: `url('${imgTalento}')` });
-    setTituloSeccionDespliegue('Que tipo de Talento puedo ragalar');
-    setParrafoSeccionDespliegue('Los aportes desde los restaurantes son parte fundamental para que toda esta hermosa labor se vuelva realidad, los restaurantes colaboran, junto con nosotros a favor de las comunidades vulnerables, no solo llevándoles alimentos.');
+    setTituloSeccionDespliegue("Que tipo de Talento puedo ragalar");
+    setParrafoSeccionDespliegue(
+      "Los aportes desde los restaurantes son parte fundamental para que toda esta hermosa labor se vuelva realidad, los restaurantes colaboran, junto con nosotros a favor de las comunidades vulnerables, no solo llevándoles alimentos."
+    );
     setContenidoModal(<FormularioTalento />);
     /* console.log(dataComoAportar.dataAportar);
     setEstadoboton(true); */
   };
 
   const changeEstadoBotonActiveMetro = () => {
-    setStyleBotonMetro({ backgroundColor: 'transparent', border: '2px solid #FD4C00', color: '#FD4C00' });
-    setStyleBotonTalento({ backgroundColor: '#FD4C00', border: 'none' });
-    setStyleBotonCapacitacion({ backgroundColor: '#FD4C00', border: 'none' });
-    setStyleBotonBanco({ backgroundColor: '#FD4C00', border: 'none' });
+    setStyleBotonMetro({
+      backgroundColor: "transparent",
+      border: "2px solid #FD4C00",
+      color: "#FD4C00",
+    });
+    setStyleBotonTalento({ backgroundColor: "#FD4C00", border: "none" });
+    setStyleBotonCapacitacion({ backgroundColor: "#FD4C00", border: "none" });
+    setStyleBotonBanco({ backgroundColor: "#FD4C00", border: "none" });
     setImgFondoSeccion({ backgroundImage: `url('${imgMetro}')` });
-    setTituloSeccionDespliegue('¿Puedo regalar cultivos desde mi hogar?');
-    setParrafoSeccionDespliegue('La respuesta es SÍ, otra manera de aportar es que regales algo de tu propia huerta casera, cualquier verdura, fruta o hierbas con los que quieras colaborar, serán un aporte para las ollas comunes de todo el País. Solo debes completar un breve formulario para nosotros poder ponernos en contacto con tigo e ir a buscar los alimentos hasta la puerta de tu hogar.');
+    setTituloSeccionDespliegue("¿Puedo regalar cultivos desde mi hogar?");
+    setParrafoSeccionDespliegue(
+      "La respuesta es SÍ, otra manera de aportar es que regales algo de tu propia huerta casera, cualquier verdura, fruta o hierbas con los que quieras colaborar, serán un aporte para las ollas comunes de todo el País. Solo debes completar un breve formulario para nosotros poder ponernos en contacto con tigo e ir a buscar los alimentos hasta la puerta de tu hogar."
+    );
     setContenidoModal(<FormularioMetroCuadrado />);
   };
 
   const changeEstadoBotonActiveCapacitacion = () => {
-    setStyleBotonCapacitacion({ backgroundColor: 'transparent', border: '2px solid #FD4C00', color: '#FD4C00' });
-    setStyleBotonTalento({ backgroundColor: '#FD4C00', border: 'none' });
-    setStyleBotonMetro({ backgroundColor: '#FD4C00', border: 'none' });
-    setStyleBotonBanco({ backgroundColor: '#FD4C00', border: 'none' });
+    setStyleBotonCapacitacion({
+      backgroundColor: "transparent",
+      border: "2px solid #FD4C00",
+      color: "#FD4C00",
+    });
+    setStyleBotonTalento({ backgroundColor: "#FD4C00", border: "none" });
+    setStyleBotonMetro({ backgroundColor: "#FD4C00", border: "none" });
+    setStyleBotonBanco({ backgroundColor: "#FD4C00", border: "none" });
     setImgFondoSeccion({ backgroundImage: `url('${imgCapacitacion}')` });
-    setTituloSeccionDespliegue('¿Con qué tipo de capacitaciones puedo aportar?');
-    setParrafoSeccionDespliegue('Si tienes conocimientos de algún área en específico y quieres colaborar para que las comunidades se empoderen y aprendan algo que les pueda dar más herramientas para poder generar ingresos por su cuenta, ésta manera de donar será la indicada. Puede ser por ejemplo algún taller para que aprendan inglés, un taller enfocado en que las personas de la tercera edad aprendan a navegar por internet o utilizar celulares, algun taller sobre cómo llevar cuentas, etc. Completando el siguiente formulario podrás inscribirte para participar capacitando a las comunidades.');
+    setTituloSeccionDespliegue(
+      "¿Con qué tipo de capacitaciones puedo aportar?"
+    );
+    setParrafoSeccionDespliegue(
+      "Si tienes conocimientos de algún área en específico y quieres colaborar para que las comunidades se empoderen y aprendan algo que les pueda dar más herramientas para poder generar ingresos por su cuenta, ésta manera de donar será la indicada. Puede ser por ejemplo algún taller para que aprendan inglés, un taller enfocado en que las personas de la tercera edad aprendan a navegar por internet o utilizar celulares, algun taller sobre cómo llevar cuentas, etc. Completando el siguiente formulario podrás inscribirte para participar capacitando a las comunidades."
+    );
     setContenidoModal(<FormularioCapacitacion />);
   };
 
   const changeEstadoBotonActiveBanco = () => {
-    setStyleBotonBanco({ backgroundColor: 'transparent', border: '2px solid #FD4C00', color: '#FD4C00' });
-    setStyleBotonTalento({ backgroundColor: '#FD4C00', border: 'none' });
-    setStyleBotonCapacitacion({ backgroundColor: '#FD4C00', border: 'none' });
-    setStyleBotonMetro({ backgroundColor: '#FD4C00', border: 'none' });
+    setStyleBotonBanco({
+      backgroundColor: "transparent",
+      border: "2px solid #FD4C00",
+      color: "#FD4C00",
+    });
+    setStyleBotonTalento({ backgroundColor: "#FD4C00", border: "none" });
+    setStyleBotonCapacitacion({ backgroundColor: "#FD4C00", border: "none" });
+    setStyleBotonMetro({ backgroundColor: "#FD4C00", border: "none" });
     setImgFondoSeccion({ backgroundImage: `url('${imgBanco}')` });
-    setTituloSeccionDespliegue('Banco de alimentos');
-    setParrafoSeccionDespliegue('Otra manera de aportar es regalando los alimentos que quieras y puedas donar desde tu hogar, puede ser cualquier tipo de alimento no perecible. Tus donaciones nos servirán para hacer más contundentes los platos de almuerzos y nos permitirá llegar a más personas. Nos debes indicar en el siguiente formulario la fecha que deseas que retiremos tu donación.');
+    setTituloSeccionDespliegue("Banco de alimentos");
+    setParrafoSeccionDespliegue(
+      "Otra manera de aportar es regalando los alimentos que quieras y puedas donar desde tu hogar, puede ser cualquier tipo de alimento no perecible. Tus donaciones nos servirán para hacer más contundentes los platos de almuerzos y nos permitirá llegar a más personas. Nos debes indicar en el siguiente formulario la fecha que deseas que retiremos tu donación."
+    );
     setContenidoModal(<FormularioBancoAlimentos />);
   };
 
@@ -109,34 +149,29 @@ const ComoAportarView = () => {
     setContenidoModal(<FormularioRestaurant />);
   };
 
-
   return (
     <div className="containeComoAportarView">
-      <RegalaUnAlmuerzo
-        contenidoInfo={'Regala un Almuerzo'}
-      />
+      <RegalaUnAlmuerzo contenidoInfo={"Regala un Almuerzo"} />
 
       <SeccionManerasAportar
-        contenidoInfo={'Otras maneras de aportar'}
-
+        contenidoInfo={"Otras maneras de aportar"}
         estiloBotonTarjetaActivaTalento={styleBotonTalento}
         estiloBotonTarjetaActivaMetro={styleBotonMetro}
         estiloBotonTarjetaActivaCapacitacion={styleBotonCapacitacion}
         estiloBotonTarjetaActivaBanco={styleBotonBanco}
-
         funcionBotonTarjetaActivaTalento={changeEstadoBotonActiveTalento}
         funcionBotonTarjetaActivaMetro={changeEstadoBotonActiveMetro}
-        funcionBotonTarjetaActivaCapacitacion={changeEstadoBotonActiveCapacitacion}
+        funcionBotonTarjetaActivaCapacitacion={
+          changeEstadoBotonActiveCapacitacion
+        }
         funcionBotonTarjetaActivaBanco={changeEstadoBotonActiveBanco}
-
       />
       <ManerasAportarSeccionDesp
         imgFondoSeccion={imgFondoSeccion}
         tituloSeccionDespliegue={tituloSeccionDespliegue}
         parrafoSeccionDespliegue={parrafoSeccionDespliegue}
-        contenidoBotonSeccionDespliegue={'Inscribete'}
+        contenidoBotonSeccionDespliegue={"Inscribete"}
         funcionBotonSeccionDespliegue={() => setModalShow(true)}
-
       />
 
       <AportarRestaurante
@@ -152,11 +187,7 @@ const ComoAportarView = () => {
         contenidoModal={contenidoModal}
         funcionBotonModal={clickBton}
       />
-
     </div>
-
-
   );
 };
 export default ComoAportarView;
-
